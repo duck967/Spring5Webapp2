24e4e4d32fdaa00298db54d53a028b36cc71b03b
Ubuntu <ubuntu@ip-172-31-28-141.us-east-2.compute.internal>
2021-12-10 15:13:30 +0000

    first commit

---
+++ src/test/java/guru/springframework/spring5webapp2/Spring5webapp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring5webapp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5webapp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
